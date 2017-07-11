b57ae75a1ee432852fe6d147bf99cd925c901098
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-10 18:02:11 -0700

    Added raw input capability and checked rating value

--- ratings.py
+++ ratings.py
@@ -8,15 +8,25 @@ restaurant_scores = open(sys.argv[1])
 
 restaurant_ratings = {}
 
-for line in restaurant_scores:
-    line = line.rstrip()
-    items = line.split(':')
-    restaurant_ratings[items[0]] = items[1]
+new_restaurant = raw_input("Name a restaurant you want to rate: ")
 
-# print restaurant_ratings
+while True:
+    new_rating = raw_input("Give this restaurant a rating between 1-5: ")
+    try:
+        if int(new_rating) in range(1, 6):
+
+            restaurant_ratings[new_restaurant] = new_rating
+            break
+        else:
+            print "Try again we need a number between 1-5."
+
+    except:
+        print "Try again we need a number between 1-5."
+
+for line in restaurant_scores:
+            line = line.rstrip()
+            items = line.split(':')
+            restaurant_ratings[items[0]] = items[1]
+
 for restaurant, rating in sorted(restaurant_ratings.items()):
     print "{} has a rating of {}.".format(restaurant, rating)
-
-# ordered_retaurants_rated = sorted(restaurant_ratings)
-
-#print ordered_retaurants_rated
